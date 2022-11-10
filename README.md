0174b46c322768c89e700a2b8bbc3cd2c62c2337
Addison Dubay <111545869+amdubay@users.noreply.github.com>
2022-11-10 09:55:14 -0500

    attempt to get this to work with react
    Still experimenting.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # voice-education
- 
+
+This project is meant to help my child develop his speech.
+
+To give a little background, my son is a late speaker.
+He has greatly improved, but he can still be a bit stubborn. If I try to do learning games with him to get him to refine his speech, not only does he lose interest, but he often flat-out refuses to participate.
+
+He loves cars and trucks, and I have used that to my advantage before. I have built racetracks on poster board with letters, numbers, pictures along the way, that he has to pronounce before he can proceed. However, it doesn't defeat his stubborness. When going through the alphabet (on the racetrack) there are certain parts of the alphabet that he refuses to deviate from the tempo of the alphabet song. For example, he refuses to go slowly through "L, M, N, O, and P". While he should be stopping at each letter and annunciating each, "L", "M", "N", "O", and "P", he will only speed through it like the song "lmnop". He does the same thing with "W and X". Additionally, he refuses to hold back when I tell him he has mis-pronounced a letter.
+
+That's where this project comes in. The end goal is to create a cool voice recognition game that uses cars, trucks, racetracks, and other imagery as well to keep my son engaged, as well as accountable to accuracy.
+
+The end product will likely be a mostly touch free experience (no mouse, keyboard, or touchscreen), with any touching solely to go to different sections, levels, or challenges. The final product flow has not fully been decided yet, however, the two competing ideas are: 1) Storyboard style. The 'game' is a story that progresses as each letter/number/word is said correctly. 2) Game-mode. For example, the user may be racing against another car. With every letter/number/word said correctly, the user's car will move ahead, with an incorrect answer, the opponent car may move ahead.
+Additionally, the product will initially use text-to-speech to ask the questions ("what is this color?"), but it may later be decided to add actual human voice-over.
+
+The product will start simple to get all the moving parts in place. This means no game or story aspect to it, will only initially ask for letters of the alphabet, will keep running score.
+
+The flow of the initial product will be as follows:
+
+1. page loads with button to start, user clicks start. (the running score also loads too, which will start at 0)
+2. A random letter of the alphabet is displayed on the screen
+3. A text-to-voice audio plays asking "what is this letter?"
+4. A 'ding' sound is played to indicate that the computer microphone is now listening.
+5. User (should) say the letter.
+   5.1 - If audio is recorded, play alt "done-ding" sound to indicate recording stopped, but successfully heard. proceed to step 5.2 or 5.3
+   5.2 - If speech to text matches the letter correctly, play text-to-voice "great job, you got it!" go to step 6. (score as correct)
+   5.3 - If speech is incorrect, play text-to-voice, "I'm sorry, I didn't hear the correct answer". Then start back to step 3. Repeat max 3 or so times, if still not said correctly, go to step 6. (score as incorrect)
+   5.4 - If no audio is detected, play alt 'bad-ding' sound to indicate recording stopped, nothing was heard. Same action as 5.3.
+6. Score as determined, start back at step 2.
---
+++ alphabet.js
@@ -0,0 +1,152 @@
+var synth = window.speechSynthesis;
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var SpeechGrammarList = SpeechGrammarList || window.webkitSpeechGrammarList;
+var SpeechRecognitionEvent =
+  SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
+var alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+var phonetic = {
+    "a": ["hey", "a"],
+    "b": ["be", "b", "bee"],
+    "c": ["see", "c", "sea"],
+    /*"d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",*/
+};
+console.log(alphabet)
+console.log(Object.keys(phonetic))
+
+var recognition = new SpeechRecognition();
+
+if (SpeechGrammarList) {
+  // SpeechGrammarList is not currently available in Safari, and does not have any effect in any other browser.
+  // This code is provided as a demonstration of possible capability. You may choose not to use it.
+  var speechRecognitionList = new SpeechGrammarList();
+  var grammar =
+    "#JSGF V1.0; grammar alphabet; public <alphabet> = " + alphabet.join(" | ") + " ;";
+  speechRecognitionList.addFromString(grammar, 1);
+  recognition.grammars = speechRecognitionList;
+}
+
+recognition.continuous = false;
+recognition.lang = "en-US";
+recognition.interimResults = false;
+recognition.maxAlternatives = 10;
+console.log(speechRecognitionList[0]);
+
+// question will eventually be randomly chosen.
+let question = "c";
+let matchQuestion = phonetic[question]
+let appendQuestion = document.getElementById("question");
+
+appendQuestion.innerHTML = question;
+document.body.onclick = function () {
+  recognition.start();
+  console.log("Ready to receive a letter.");
+};
+
+recognition.onresult = function (event) {
+  // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
+  // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
+  // It has a getter so it can be accessed like an array
+  // The first [0] returns the SpeechRecognitionResult at the last position.
+  // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
+  // These also have getters so they can be accessed like arrays.
+  // The second [0] returns the SpeechRecognitionAlternative at position 0.
+  // We then return the transcript property of the SpeechRecognitionAlternative object
+  var letter = event.results[0][0].transcript;
+  console.log("Result received: " + letter + ".");
+  console.log("Confidence: " + event.results[0][0].confidence);
+  console.log(letter);
+  console.log(event.results[0])
+  let result = ""
+  console.log(event.results[0].length)
+  console.log(event.results[0][1])
+  console.log(phonetic[question])
+  let correct = false;
+  console.log(correct)
+  for (i = 0; i <event.results[0].length; i++) {
+    for (c=0; c < matchQuestion.length; c++) {
+        if (event.results[0][i].transcript.toLowerCase() == matchQuestion[c]) {
+            correct = true;
+            console.log(correct)
+            break;
+        }
+    }
+console.log(correct)
+        
+    /*if (event.results[0][i].transcript.length == 1) {
+            result = event.results[0][i].transcript
+            break;
+        }*/
+  }
+
+  console.log("result = " + result);
+
+  for (i = 0; i < alphabet.length; i++) {
+    //console.log("in loop, current " + alphabet[i]);
+    if (letter.includes(alphabet[i])) {
+      //console.log("match found " + alphabet[i]);
+    }
+  }
+};
+
+recognition.onspeechend = function () {
+  recognition.stop();
+};
+
+recognition.onnomatch = function (event) {
+  console.log("I didn't recognise that letter.");
+};
+
+recognition.onerror = function (event) {
+  console.log("Error occurred in recognition: " + event.error);
+};
